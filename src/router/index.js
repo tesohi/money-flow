671bee752b827b2ad6b8bacbfb53e906cdab1012
tesohi <tesohi99@gmail.com>
2021-05-09 21:45:01 +0300

    created base pages

--- src/router/index.js
+++ src/router/index.js
@@ -8,7 +8,56 @@ const routes = [
   {
     path: '/',
     name: 'Home',
+    meta: {layout: 'main'},
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Login.vue')
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    meta: {layout: 'main'},
+    component: () => import('../views/Categories.vue')
+  },
+  {
+    path: '/detail-record',
+    name: 'Detail-record',
+    meta: {layout: 'main'},
+    component: () => import('../views/DetailRecord.vue')
+  },
+  {
+    path: '/history',
+    name: 'History',
+    meta: {layout: 'main'},
+    component: () => import('../views/History.vue')
+  },
+  {
+    path: '/planning',
+    name: 'Planning',
+    meta: {layout: 'main'},
+    component: () => import('../views/Planning.vue')
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    meta: {layout: 'main'},
+    component: () => import('../views/Profile.vue')
+  },
+  {
+    path: '/record',
+    name: 'Record',
+    meta: {layout: 'main'},
+    component: () => import('../views/Record.vue')
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    meta: {layout: 'empty'},
+    component: () => import('../views/Register.vue')
   }
 ]
 
